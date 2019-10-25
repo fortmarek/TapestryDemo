e0a9fb8569e0801562050da2678f1f4c39907d9d
Marek Fořt <marek.fort@ackee.cz>
2019-10-25 14:00:40 +0200

    Remove tapestries.

--- Tapestries/Sources/TapestryConfig/TapestryConfig.swift
+++
@@ -1,9 +0,0 @@
-import PackageDescription
-
-let config = TapestryConfig(release: Release(actions: [.pre(.docsUpdate),
-                                                       .pre(.dependenciesCompatibility([.spm(.all)]))],
-                                             add: ["README.md",
-                                                   "TapestryDemo.podspec",
-                                                   "CHANGELOG.md"],
-                                             commitMessage: "Version \(Argument.version)",
-                                             push: true))
