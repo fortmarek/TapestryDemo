e0e32e11365bf72bf09392c20aad2f37b030c77a
Marek Fořt <marek.fort@ackee.cz>
2020-07-11 16:40:31 +0200

    Add back tapestry steps.

--- TapestryConfig.swift
+++ TapestryConfig.swift
@@ -4,14 +4,14 @@ let config = TapestryConfig(
     release: Release(
         actions: [
             .pre(.docsUpdate),
-            // .pre(.dependenciesCompatibility([.spm(.all)])),
-            // .post(tool: "zip", arguments: ["-r", "license.zip", "LICENSE"]),
-            // .post(.githubRelease(owner: "fortmarek", repository: "TapestryDemo", assetPaths: ["build.zip"])),
+            .pre(.dependenciesCompatibility([.spm(.all)])),
+            .post(tool: "zip", arguments: ["-r", "license.zip", "LICENSE"]),
+            .post(.githubRelease(owner: "fortmarek", repository: "TapestryDemo", assetPaths: ["build.zip"])),
         ],
         add: [
-            // "README.md",
-            // "TapestryDemo.podspec",
-            // "CHANGELOG.md"
+            "README.md",
+            "TapestryDemo.podspec",
+            "CHANGELOG.md"
         ],
         commitMessage: "Version \(Argument.version)",
         push: false
