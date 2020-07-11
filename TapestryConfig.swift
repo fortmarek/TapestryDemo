190171eed60bd5c1c77aadba8446ecf3c762bdf3
Marek Fořt <marek.fort@ackee.cz>
2020-07-11 12:39:00 +0200

    Add tapestry github action.

--- TapestryConfig.swift
+++ TapestryConfig.swift
@@ -1,18 +1,19 @@
 import TapestryDescription
 
 let config = TapestryConfig(
-      release: Release(
-            actions: [
-                  .pre(.docsUpdate),
-                  // .pre(.dependenciesCompatibility([.spm(.all)])),
-                  .post(.githubRelease(owner: "fortmarek", repository: "TapestryDemo")),
-            ],
-            add: [
-                  "README.md",
-                  "TapestryDemo.podspec",
-                  "CHANGELOG.md"
-            ],
-            commitMessage: "Version \(Argument.version)",
-            push: true
-      )
+    release: Release(
+        actions: [
+            .pre(.docsUpdate),
+            // .pre(.dependenciesCompatibility([.spm(.all)])),
+            .post(tool: "zip", arguments: ["-r", "build.zip", ".build"]),
+            .post(.githubRelease(owner: "fortmarek", repository: "TapestryDemo", assetPaths: ["build.zip"])),
+        ],
+        add: [
+            "README.md",
+            "TapestryDemo.podspec",
+            "CHANGELOG.md"
+        ],
+        commitMessage: "Version \(Argument.version)",
+        push: true
+    )
 )
