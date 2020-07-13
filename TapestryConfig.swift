e780d65f6b72e3f03ceb59a34d81a258413a1d40
Marek Fořt <marek.fort@ackee.cz>
2020-07-13 19:18:54 +0200

    Temp disable checking compatibility.

--- TapestryConfig.swift
+++ TapestryConfig.swift
@@ -4,7 +4,7 @@ let config = TapestryConfig(
     release: Release(
         actions: [
             .pre(.docsUpdate),
-            .pre(.dependenciesCompatibility([.spm(.all)])),
+            // .pre(.dependenciesCompatibility([.spm(.all)])),
             .post(tool: "zip", arguments: ["-r", "license.zip", "LICENSE"]),
             .post(.githubRelease(owner: "fortmarek", repository: "TapestryDemo", assetPaths: ["license.zip"])),
         ],
